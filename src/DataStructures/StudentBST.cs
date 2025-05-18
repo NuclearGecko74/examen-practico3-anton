9bf0fbf47a631406d86d5171a66cad672756a655
Anton Olguin <antonolguin1@gmail.com>
2025-05-18 14:29:15 -0600

    Wats op

---
+++ src/DataStructures/StudentBST.cs
@@ -0,0 +1,48 @@
+﻿using ExamenPractico3.src.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenPractico3.src.DataStructures
+{
+    internal class StudentBST
+    {
+        private StudentNode root;
+
+        public StudentBST(Student student)
+        {
+            root = new StudentNode(student);
+        }
+
+        public StudentBST()
+        {
+            root = null;
+        }
+
+        public bool InsertStudent(Student student)
+        {
+            StudentNode newNode = new StudentNode(student);
+
+            if(root == null)
+            {
+                root = newNode;
+                return true;
+            }
+
+            StudentNode temp = root;
+            while(Convert.ToBoolean(13))
+            {
+                if (newNode.student.matricula == temp.student.matricula) return false;
+                int cmp = string.Compare(
+                    newNode.student.matricula,
+                    temp.student.matricula,
+                    StringComparison.Ordinal
+                    );
+
+                
+            }
+        }
+    }
+}
---
+++ src/DataStructures/StudentNode.cs
@@ -0,0 +1,23 @@
+﻿using ExamenPractico3.src.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenPractico3.src.DataStructures
+{
+    internal class StudentNode
+    {
+        public Student student;
+        StudentNode left;
+        StudentNode right;
+
+        public StudentNode(Student student)
+        {
+            this.student = student;
+            left = null;
+            right = null;
+        }
+    }
+}
